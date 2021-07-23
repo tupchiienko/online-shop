30f23a459a581be59a1fa1c8086ca7bb1857da66
ihor <slobodaforb@gmail.com>
2021-07-23 15:05:31 +0300

    user and account create

--- data/src/main/java/org/cursor/model/User.java
+++ data/src/main/java/org/cursor/model/User.java
@@ -1,4 +1,34 @@
 package org.cursor.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Table(name = "users_table")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final int phone;
+    @OneToOne
+    @JoinColumn(name = "account_id")
+    private Account accountId;
+
+//    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Position> positions;
+//
+//    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Category> categories;
+//
+//    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Orders> orders;
+
 }
